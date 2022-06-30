0010149f8329a9e7e350f2b288c497efabfe0e07
fs-atGithub <filip.sostaric@proton.me>
2022-06-30 20:31:29 +0200

    clear input

--- src/App.js
+++ src/App.js
@@ -1,64 +1,25 @@
 import "./App.css";
-import { InputElement } from "./components/InputElement";
-import Button from "./components/Button";
-import FormError from "./components/FormError";
-import FormMessage from "./components/FormMessage";
-import { User } from "./components/User";
-import { Counter } from "./components/Counter";
 import { useState } from "react";
+import { SignInPage } from "./pages/SignInPage";
+import { ChatRoomPage } from "./pages/ChatRoomPage";
 
 function App() {
-  const [formState, setFormState] = useState({ username: '', password: '' });
   const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    if (formState.password === 'password') {
-      setUser(formState);
-      setError(null);
-    } else {
-      setError(true);
-    }
+  const storeUser = (user) => {
+    setUser(user);
   }
 
-  const handleChange = (event) => {
-    setFormState((state) => ({
-          ...state,
-          [event.target.name]: event.target.value,
-    }));
-  }
-
-  const signOut = () => {
+  const clearUser = () => {
     setUser(null);
-    setError(null);
   }
 
   const isSignedIn = user !== null;
-  const showErrorMessage = error !== null;
-  const showFormMessage = user !== null;
 
   return (
     <div>
-      <Counter initialValue={0} />
-      <Counter initialValue={100} />
-      {isSignedIn && <Button type="button" onClick={signOut}>Sign out</Button>}
-      {isSignedIn && <User src="/avatar.svg" username={user.username} />}
-      {!isSignedIn && <form className="form" onSubmit={handleSubmit}>
-        <div className="form-field">
-          <InputElement name="username" label="Username" type="text" onChange={handleChange} />
-        </div>
-        <div className="form-field">
-          <InputElement name="password" label="Password" type="password" onChange={handleChange} />
-        </div>
-        <div className="form-field">
-          <Button type="submit">Sign in</Button>
-          <Button type="reset">Reset</Button>
-        </div>
-        <FormError visible={showErrorMessage} />
-        <FormMessage visible={showFormMessage} />
-      </form>}
+      {isSignedIn && <ChatRoomPage onSignOut={clearUser} user={user} />}
+      {!isSignedIn && <SignInPage onSignIn={storeUser} />}
     </div>
   );
 }
---
+++ src/pages/ChatRoomPage.jsx
@@ -0,0 +1,46 @@
+import Button from "../components/Button";
+import { InputElement } from "../components/InputElement";
+import { User } from "../components/User";
+import { useState } from "react";
+
+export function ChatRoomPage(props) {
+  const [formState, setFormState] = useState({ message: '' });
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (formState.message !== '') {
+      setMessages((state) => [...state, formState]);
+      setFormState({ message: '' });
+    }
+  }
+  
+  const handleChange = (event) => {
+    setFormState((state) => ({
+          ...state,
+          [event.target.name]: event.target.value,
+    }));
+  }
+
+  console.log(messages);
+
+  return (
+    <div>
+      <Button type="button" onClick={props.onSignOut}>Sign out</Button>
+      <User src="/avatar.svg" username={props.user.username} />
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-field">
+          <InputElement name="message"
+            label="Message"
+            type="text"
+            onChange={handleChange}
+            value={formState.message} />
+        </div>
+        <div className="form-field">
+          <Button type="submit">Send</Button>
+        </div>
+      </form>
+    </div>
+  );
+}
