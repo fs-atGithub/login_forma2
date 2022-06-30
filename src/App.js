ef31dcefeea8ff2459b9647c2d20ce398635a7b7
fs-atGithub <filip.sostaric@proton.me>
2022-06-30 16:55:29 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -5,28 +5,60 @@ import FormError from "./components/FormError";
 import FormMessage from "./components/FormMessage";
 import { User } from "./components/User";
 import { Counter } from "./components/Counter";
+import { useState } from "react";
 
 function App() {
-  const isSignedIn = true;
+  const [formState, setFormState] = useState({ username: '', password: '' });
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (formState.password === 'password') {
+      setUser(formState);
+      setError(null);
+    } else {
+      setError(true);
+    }
+  }
+
+  const handleChange = (event) => {
+    setFormState((state) => ({
+          ...state,
+          [event.target.name]: event.target.value,
+    }));
+  }
+
+  const signOut = () => {
+    setUser(null);
+    setError(null);
+  }
+
+  const isSignedIn = user !== null;
+  const showErrorMessage = error !== null;
+  const showFormMessage = user !== null;
 
   return (
     <div>
-      <Counter />
-      {isSignedIn && <User src="/avatar.svg" username="Domagoj" />}
-      <form className="form">
+      <Counter initialValue={0} />
+      <Counter initialValue={100} />
+      {isSignedIn && <Button type="button" onClick={signOut}>Sign out</Button>}
+      {isSignedIn && <User src="/avatar.svg" username={user.username} />}
+      {!isSignedIn && <form className="form" onSubmit={handleSubmit}>
         <div className="form-field">
-          <InputElement label="Username" type="text" />
+          <InputElement name="username" label="Username" type="text" onChange={handleChange} />
         </div>
         <div className="form-field">
-          <InputElement label="Password" type="password" />
+          <InputElement name="password" label="Password" type="password" onChange={handleChange} />
         </div>
         <div className="form-field">
-          <Button type="button">Sign in</Button>
+          <Button type="submit">Sign in</Button>
           <Button type="reset">Reset</Button>
         </div>
-        <FormError visible={!isSignedIn} />
-        <FormMessage visible={isSignedIn} />
-      </form>
+        <FormError visible={showErrorMessage} />
+        <FormMessage visible={showFormMessage} />
+      </form>}
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,26 +1,25 @@
 import "./Counter.css";
 import { useState } from "react";
 
-export function Counter() {
-  const counterState = useState(0);
+export function Counter(props) {
+  // const [counter, setCounter] = useState(props.initialValue);
+  const counterState = useState(props.initialValue);
   const counter = counterState[0];
   const setCounter = counterState[1];
 
-  const plus = () => {
-    setCounter((state) => ++state);
-  };
-  const minus = () => {
+  const handleDecrease = () => {
     setCounter((state) => --state);
-  };
+  }
+
+  const handleIncrease = () => {
+    setCounter((state) => ++state);
+  }
+
   return (
     <div>
-      <button className="counter" onClick={minus}>
-        -
-      </button>
+      <button className="counter" onClick={handleDecrease}>-</button>
       <span>{counter}</span>
-      <button className="counter" onClick={plus}>
-        +
-      </button>
+      <button className="counter" onClick={handleIncrease}>+</button>
     </div>
   );
 }
